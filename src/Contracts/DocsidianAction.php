e115320f53997d76be1ad490724bde1642c43734
Len Woodward <Len@ProjektGopher.com>
2025-02-03 17:34:55 -0800

    adds DocsidianAction contract

---
+++ src/Contracts/DocsidianAction.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ArtisanBuild\Docsidian\Contracts;
+
+use ArtisanBuild\Docsidian\DocsidianPage;
+use Closure;
+
+interface DocsidianAction
+{
+    /**
+     * @param Closure(DocsidianPage):DocsidianPage $next
+     */
+    public function __invoke(DocsidianPage $page, Closure $next): DocsidianPage;
+}
